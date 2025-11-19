c1b9c8c3ab6b085dc109cefe0e6941d90bb1d0b2
binbinmods <binbinmods@gmail.com>
2025-11-19 17:02:28 -0500

    Initial Setup

---
+++ ExpNormalizationFunctions.cs
@@ -0,0 +1,93 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+// using Obeliskial_Content;
+// using Obeliskial_Essentials;
+using System.IO;
+using static UnityEngine.Mathf;
+using UnityEngine.TextCore.LowLevel;
+using static ExpNormalization.Plugin;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+namespace ExpNormalization
+{
+    public class ExpNormalizationFunctions
+    {
+        public static Dictionary<int, SortedList<int, int>> DifficultyExpDictionary = [];
+
+        public static int GetHighestEnemyDifficulty(Enums.CombatTier combatTier)
+        {
+            int highestDifficulty = 0;
+            // List<EnemyData> enemiesInCombat = AtOManager.Instance.GetCurrentCombatData().GetAllEnemiesInCombat();
+            // foreach (EnemyData enemy in enemiesInCombat)
+            // {
+            //     int enemyDifficulty = (int)enemy.GetDifficultyForTier(combatTier);
+            //     if (enemyDifficulty > highestDifficulty)
+            //     {
+            //         highestDifficulty = enemyDifficulty;
+            //     }
+            // }
+            return highestDifficulty;
+        }
+
+        public static int GetChampionDifficulty(Enums.CombatTier combatTier)
+        {
+            int highestDifficulty = 0;
+            // List<EnemyData> enemiesInCombat = AtOManager.Instance.GetCurrentCombatData().GetAllEnemiesInCombat();
+            // foreach (EnemyData enemy in enemiesInCombat)
+            // {
+            //     int enemyDifficulty = (int)enemy.GetDifficultyForTier(combatTier);
+            //     if (enemyDifficulty > highestDifficulty)
+            //     {
+            //         highestDifficulty = enemyDifficulty;
+            //     }
+            // }
+            return highestDifficulty;
+        }
+
+        public static int[] GetHighestExp(int difficulty, bool isAct3 = false)
+        {
+            List<int> highestExp = [0, 0, 0, 0];
+
+            SortedList<int, int> expList = [];
+            // expList.Last().Key;
+
+            // List<EnemyData> enemiesInCombat = AtOManager.Instance.GetCurrentCombatData().GetAllEnemiesInCombat();
+            // foreach (EnemyData enemy in enemiesInCombat)
+            // {
+            //     int enemyDifficulty = (int)enemy.GetDifficultyForTier(combatTier);
+            //     if (enemyDifficulty > highestDifficulty)
+            //     {
+            //         highestDifficulty = enemyDifficulty;
+            //     }
+            // }
+            return highestExp.ToArray();
+        }
+
+        public static int GetChampionExp(int difficulty, bool isAct3 = false)
+        {
+            int championExp = 0;
+            // List<EnemyData> enemiesInCombat = AtOManager.Instance.GetCurrentCombatData().GetAllEnemiesInCombat();
+            // foreach (EnemyData enemy in enemiesInCombat)
+            // {
+            //     int enemyDifficulty = (int)enemy.GetDifficultyForTier(combatTier);
+            //     if (enemyDifficulty > highestDifficulty)
+            //     {
+            //         highestDifficulty = enemyDifficulty;
+            //     }
+            // }
+            return championExp;
+        }
+
+        public static void SetDifficultyExpDictionary(Dictionary<string, NPCData> npcDictionary)
+        {
+
+        }
+
+
+
+    }
+}
+
---
+++ ExpNormalizationPatches.cs
@@ -0,0 +1,76 @@
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+// using static Obeliskial_Essentials.Essentials;
+using System;
+using static ExpNormalization.Plugin;
+using static ExpNormalization.CustomFunctions;
+using static ExpNormalization.ExpNormalizationFunctions;
+using System.Collections.Generic;
+using static Functions;
+using UnityEngine;
+// using Photon.Pun;
+using TMPro;
+using System.Linq;
+using System.Xml.Serialization;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Diagnostics;
+using UnityEngine.Rendering;
+// using Unity.TextMeshPro;
+
+// Make sure your namespace is the same everywhere
+namespace ExpNormalization
+{
+
+    [HarmonyPatch] // DO NOT REMOVE/CHANGE - This tells your plugin that this is part of the mod
+
+    public class ExpNormalizationPatches
+    {
+        public static bool devMode = false; //DevMode.Value;
+        public static bool bSelectingPerk = false;
+        public static bool IsHost()
+        {
+            return GameManager.Instance.IsMultiplayer() && NetworkManager.Instance.IsMaster();
+        }
+
+
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(AtOManager), "GetExperienceFromCombat")]
+        public static void GetExperienceFromCombat(ref int __result)
+        {
+            if (!AtOManager.Instance)
+            {
+                return;
+            }
+            CombatData combat = AtOManager.Instance.GetCurrentCombatData();
+            if (combat == null)
+            {
+                LogDebug($"Exp Normalization: No combat data found for {AtOManager.Instance.currentMapNode}.");
+                return;
+            }
+            int highestDifficulty = GetHighestEnemyDifficulty(combat.CombatTier);
+            int championDifficulty = GetChampionDifficulty(combat.CombatTier);
+            bool isAct3 = AtOManager.Instance.GetActNumberForText() == 3;
+            int[] highestExp = GetHighestExp(highestDifficulty, isAct3: isAct3);
+            int championExp = GetChampionExp(championDifficulty, isAct3: isAct3);
+            __result = highestExp.Sum() + championExp;
+            LogDebug($"Exp Normalization Applied: Combat {combat.CombatId}, Highest Difficulty {highestDifficulty}, Champion Difficulty {championDifficulty}, Total Exp {__result}");
+            return;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Globals), "CreateGameContent")]
+        public static void CreateGameContentPostfix()
+        {
+
+            SetDifficultyExpDictionary(Globals.Instance.NPCs);
+        }
+
+
+
+    }
+}
